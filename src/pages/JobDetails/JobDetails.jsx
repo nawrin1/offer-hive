eca9098bd352fb7baaa21e0c9cb009ffbceb135c
Rakibul88 <rakibulhasan999000@gmail.com>
2023-11-08 14:17:13 +0600

    place your bids form done

--- src/pages/JobDetails/JobDetails.jsx
+++ src/pages/JobDetails/JobDetails.jsx
@@ -4,12 +4,43 @@ import {FaDollarSign} from 'react-icons/fa6'
 import img1 from '../../assets/7484902.jpg'
 import { useContext } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
+import Swal from 'sweetalert2'
 const JobDetails = () => {
     const data=useLoaderData()
     console.log(data,"dattt")
     const {description,deadline,jobtitle,price,_id,email}=data
     console.log(email)
     const {user}=useContext(AuthContext)
+    const handleBids=e=>{
+        e.preventDefault()
+        const price=e.target.price.value
+        const deadline=e.target.deadline.value
+        const userEmail=e.target.email.value
+        const buyerEmail=e.target.bEmail.value
+        console.log(price,deadline,userEmail,buyerEmail)
+        const bids={price,deadline,userEmail,buyerEmail}
+        fetch('http://localhost:3000/allBids', {
+            method: 'POST', 
+            headers: {
+                'content-type': 'application/json'
+            }, 
+            body: JSON.stringify(bids)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.insertedId){
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: 'Bid Successfully ',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+            }
+        })
+
+    }
     return (
         <div className="min-h-screen my-12 max-w-6xl mx-auto">
             <h2 className="text-center font-bold font-Sora text-5xl text-purple-900 mb-20">{jobtitle}</h2>
@@ -31,7 +62,7 @@ const JobDetails = () => {
                 <h2 className="text-4xl font-Sora font-medium text-center mb-10 ">Place Your Bid</h2>
                 <div className="bg-[#c4dde9] p-16">
             
-            <form>
+            <form onSubmit={handleBids}>
                 
                 <div className="md:flex text-2xl font-Sora">
                     <div className="form-control md:w-1/2 w-full lg:w-1/2">
@@ -47,7 +78,7 @@ const JobDetails = () => {
                             <span className="">Deadline</span>
                         </label>
                         <label className="input-group">
-                            <input type="date" name="date" placeholder="deadline" className="input input-bordered w-full" />
+                            <input type="date" name="deadline" placeholder="deadline" className="input input-bordered w-full" />
                         </label>
                     </div>
                 </div>
@@ -58,7 +89,7 @@ const JobDetails = () => {
                             <span className="">Email</span>
                         </label>
                         <label className="input-group">
-                            <input type="email" name="bEmail" defaultValue={user.email} placeholder="your email" disabled className="input input-bordered w-full" />
+                            <input type="email" name="email" defaultValue={user.email} placeholder="your email" disabled className="input input-bordered w-full" />
                         </label>
                     </div>
                     <div className="form-control md:w-1/2 ml-2 w-full lg:w-1/2">
@@ -66,14 +97,25 @@ const JobDetails = () => {
                             <span className="">Buyer Email</span>
                         </label>
                         <label className="input-group" >
-                            <input type="email" name="taste" defaultValue={email} placeholder="buyer email" className="input input-bordered w-full" disabled/>
+                            <input type="email" name="bEmail" defaultValue={email} placeholder="buyer email" className="input input-bordered w-full" disabled/>
                         </label>
                     </div>
                 </div>
-                
+                {/* {
+                    
+                 user.email==email <input type="submit" value="Bid On The Project" className="btn btn-outline w-full"/>: <input type="submit" value="Bid On The Project" className="btn btn-outline w-full"/>
+                }
+                 */}
+                            {
+            user.email === email ? (
+                <input type="submit" value="Bid On The Project" className="btn btn-outline w-full" disabled />
+            ) : (
+                <input type="submit" value="Bid On The Project" className="btn btn-outline w-full" />
+            )
+            }
                
-               
-                <input type="submit" value="Bid On The Project" className="btn btn-outline w-full"/>
+{/*                
+                <input type="submit" value="Bid On The Project" className="btn btn-outline w-full"/> */}
 
             </form>
         </div>
