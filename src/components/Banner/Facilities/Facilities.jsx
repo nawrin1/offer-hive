7b4580c64fe5fb0108d62975a8a00742b06c9762
Rakibul88 <rakibulhasan999000@gmail.com>
2023-11-09 14:04:38 +0600

    facilities and how it works section responsive done

--- src/components/Banner/Facilities/Facilities.jsx
+++ src/components/Banner/Facilities/Facilities.jsx
@@ -2,10 +2,11 @@ import {MdOutlinePlaylistAddCheckCircle} from "react-icons/md"
 
 const Facilities = () => {
     return (
-<div><h2 className="text-center lg:text-5xl text-2xl md:text-3xl font-bold font-Sora lg:mb-10 mb-6 ">Our Facilities</h2>
+<div className="max-w-6xl mx-auto mb-20"><h2 className="text-center lg:text-5xl text-2xl md:text-3xl font-bold font-Sora lg:mb-10 mb-6 ">Our Facilities</h2>
+<hr className="w-full h-[1px] bg-orange-400 mb-3 lg:block hidden"></hr>
 <div className="flex max-w-6xl mx-auto justify-center p-8">
-            <div className="lg:text-2xl text-[14px] md:[text-16px] text-blue-950 font-Sora font-medium">
-                <ul className="list-disc">
+            <div className="lg:text-2xl text-[14px] md:[text-16px] text-blue-900 font-Sora font-medium mb-10">
+                <ul className="list-disc space-y-4">
                     <li>A user-friendly search function to find relevant job listings.</li>
                     <li>Track the status of job applications.</li>
                     <li>Promote job postings for greater visibility.</li>
